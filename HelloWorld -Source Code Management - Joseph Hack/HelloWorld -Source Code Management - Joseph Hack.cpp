4a41fa807d674dd23fd5fdff29d72dd5a8f24164
hackj <hackj@DESKTOP-HNA7VJS>
2024-10-27 21:01:51 -0400

    Add project files.

---
+++ HelloWorld -Source Code Management - Joseph Hack/HelloWorld -Source Code Management - Joseph Hack.cpp
@@ -0,0 +1,15 @@
+// HelloWorld -Source Code Management - Joseph Hack.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// October 27, 2024
+// CIS1202
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
+
